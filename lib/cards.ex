233d574f88709f08ec746957a0c26cb3b7e4a79d
Vincent Corniere <vincentcorniere@MacBook-Pro-de-Vincent.local>
2021-01-07 17:15:28 +0100

    add shuffle method

--- lib/cards.ex
+++ lib/cards.ex
@@ -1,6 +1,12 @@
 defmodule Cards do
-  @spec create_deck :: [<<_::24, _::_*16>>, ...]
+  @spec create_deck :: list()
   def create_deck do
     ["Ace", "Two", "Three"]
+  end
+
+  @spec shuffle(list()) :: list()
+  def shuffle(deck) do
+    deck
+    |> Enum.shuffle
   end
 end
--- test/cards_test.exs
+++ test/cards_test.exs
@@ -2,7 +2,11 @@ defmodule CardsTest do
   use ExUnit.Case
   doctest Cards
 
-  test "greets the world" do
+  test "generate deck of card" do
     assert Cards.create_deck() == ["Ace", "Two", "Three"]
+  end
+
+  test "shuffle deck of card" do
+    assert Cards.shuffle(["Ace", "Two"]) in [["Ace", "Two"], ["Two", "Ace"]]
   end
 end
